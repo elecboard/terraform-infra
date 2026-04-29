e53b6c61c7f2a1125a26390958ac885dfd0be5f6
Edna <edna@electronicboard.es>
2026-04-29 13:43:13 +0200

    Upd: added b2s and config scripts to lambda-scripts

---
+++ infrastructure/lambda-scripts/config/db_connection.py
@@ -0,0 +1,27 @@
+import os
+import mysql.connector
+
+from .env_utils import load_env
+
+load_env()
+
+DB_CONFIG = {
+    'user': os.getenv("DB_USER"),
+    'password': os.getenv("DB_PASSWORD"),
+    'host': os.getenv("DB_HOST"),
+    'database': os.getenv("DB_NAME")
+}
+
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(
+            **DB_CONFIG,
+            charset='utf8mb4',
+            use_unicode=True
+        )
+        conn.autocommit = False
+        cursor = conn.cursor(dictionary=True)
+        return conn, cursor
+    except mysql.connector.Error as err:
+        print("DB Connection Error:", err)
+        exit(1)
