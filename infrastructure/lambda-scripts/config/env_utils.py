b2cd4015dd75945b3baea813825f5f467b4102bd
Edna <edna@electronicboard.es>
2026-06-04 12:58:45 +0200

    Fix: deleted deprecated files (lambda-scripts used to adapt source code to lambda)

--- infrastructure/lambda-scripts/config/env_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-
-def load_env() -> str:
-    env_path = os.getenv("ENV_PATH") or os.path.join(os.path.dirname(__file__), ".env")
-    load_dotenv(env_path)
-    return env_path
